ee483ebbdd5f6ce9a2cc001e5f6fb119a408eaef
alinasr783 <alinasreldin783@gmail.com>
2025-10-04 01:38:18 +0000

    set up

---
+++ src/pages/landing.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/landing/jsx/Header'
+
+export default function Landing(){
+  return(
+    <div className='landing'>
+      <Header />
+    </div>
+  )
+}
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    allowedHosts: ['3100c0b7-b757-45e5-abfb-c6edb1b7efb5-00-94yyiwel8ck7.janeway.replit.dev']
+  },
+})
